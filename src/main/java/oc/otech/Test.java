b3f512943550cb6755d4598c25aea579ec8fce4a
Bruno SESSEPA <Bruno@otec.co>
2022-03-06 17:03:42 +0100

    Comments

--- src/main/java/oc/otech/Test.java
+++ src/main/java/oc/otech/Test.java
@@ -1,6 +1,10 @@
+/**
+ * Comment
+ */
 package oc.otech;
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hello World !");
+        //----------------------------------
+        System.out.println("Hello World new version!");
     }
 }
